d2bb081ddbcea258453c8403dce4bf0caef5679d
JP <102888534+normalize-vectors@users.noreply.github.com>
2024-04-17 18:38:41 -0700

    point function

--- main/main.py
+++ main/main.py
@@ -1,6 +1,9 @@
+import time
 import RPi.GPIO as GPIO
 from stepper import StepperDriver
 import secrets
+import distance
+
 
 # Motor driver  pins
 PIN_PITCH_STEP = 21
@@ -19,9 +22,6 @@ STEPDELAY_MED = 0.005
 STEPDELAY_SLOW = 0.01
 
 STEP_DEGREES = 1.8  # 1.8 degrees per step for full stepping
-
-MAX_STEPS_PITCH = int(1+90 / STEP_DEGREES)
-MAX_STEPS_YAW = int(1+180 / STEP_DEGREES)
 
 
 class Engine:
@@ -48,12 +48,27 @@ class Engine:
         self.pitch_motor = StepperDriver(PIN_PITCH_DIR, PIN_PITCH_STEP)
         self.yaw_motor = StepperDriver(PIN_YAW_DIR, PIN_YAW_STEP)
 
+        # Position of each limit in steps
+        self.pitch_upper_limit = None
+        self.pitch_lower_limit = None
+        self.yaw_left_limit = None
+        self.yaw_right_limit = None
+
+        self.pitch_axis_degrees = None
+        self.yaw_axis_degrees = None
+
         self.device_lat = secrets.device_lat  # Degrees, latitude of the device
         self.device_long = secrets.device_lon  # Degrees, longitude of the device
         self.device_alt = secrets.device_alt  # meters, height of the device above sea level, https://en-us.topographic-map.com/
 
-    def ac_position():
-        # to do
+    def current_pitch(self):
+        return (self.pitch_motor.steps - self.pitch_lower_limit)*STEP_DEGREES
+
+    def current_yaw(self):
+        return (self.yaw_motor.steps - self.yaw_left_limit)*STEP_DEGREES
+
+    def ac_position(self):
+        # TODO
         a_lat, a_lon, a_alt = None, None, None
         return a_lat, a_lon, a_alt
 
@@ -98,15 +113,28 @@ class Engine:
 
             return limit
 
-        # Find the limit, in steps, for each direction of each axis
-        self.pitch_upper_limit = find_limit(motor=self.pitch_motor, limit_switch=PIN_PITCH_UPPER, clockwise=True, max_steps=MAX_STEPS_PITCH)
-        self.pitch_lower_limit = find_limit(motor=self.pitch_motor, limit_switch=PIN_PITCH_LOWER, clockwise=False, max_steps=MAX_STEPS_PITCH)
+        # Guesses, not reliable numbers
+        max_steps_pitch = int(100 / STEP_DEGREES)
+        max_steps_yaw = int(200 / STEP_DEGREES)
+
+        # Find the limits, in steps, for the pitch axis
+        self.pitch_upper_limit = find_limit(motor=self.pitch_motor, limit_switch=PIN_PITCH_UPPER, clockwise=True, max_steps=max_steps_pitch)
+        self.pitch_lower_limit = find_limit(motor=self.pitch_motor, limit_switch=PIN_PITCH_LOWER, clockwise=False, max_steps=max_steps_pitch)
         # The pitch axis should now be at 0 degrees (horizontal)
 
-        self.yaw_left_limit = find_limit(motor=self.yaw_motor, limit_switch=PIN_YAW_RIGHT, clockwise=True, max_steps=MAX_STEPS_YAW)
-        self.yaw_right_limit = find_limit(motor=self.yaw_motor, limit_switch=PIN_YAW_RIGHT, clockwise=False, max_steps=MAX_STEPS_YAW)
+        # Record the total travel possible on the pitch axis
+        self.pitch_axis_degrees = (self.pitch_upper_limit - self.pitch_lower_limit)*STEP_DEGREES
 
-        # Move the yaw axis to 0 degrees
+        # Find the limits, in steps, for the yaw axis
+        self.yaw_left_limit = find_limit(motor=self.yaw_motor, limit_switch=PIN_YAW_RIGHT, clockwise=True, max_steps=max_steps_yaw)
+        self.yaw_right_limit = find_limit(motor=self.yaw_motor, limit_switch=PIN_YAW_RIGHT, clockwise=False, max_steps=max_steps_yaw)
+
+        # Record the total travel possible on the yaw axis
+        self.yaw_axis_degrees = (self.yaw_left_limit - self.yaw_right_limit)*STEP_DEGREES
+
+        # Move the yaw axis to the center of the axis
+        steps = int(self.yaw_axis_degrees/(2*STEP_DEGREES))
+        self.yaw_motor.motor_go(clockwise=True, steps=steps)
 
     def thingsboard_stuff(self):
         # Update thingsboard info and check if any buttons have been pressed
@@ -115,8 +143,30 @@ class Engine:
     def point(self, camera):
         # Point the camera at the current being tracked aircraft.
 
+        a_lat, a_lon, a_alt = self.ac_position()
+
+        desired_pitch = distance.find_pitch(self.device_lat, self.device_lon, self.device_alt, a_lat, a_lon, a_alt)
+
+        delta_pitch = desired_pitch - self.current_pitch()
+
+        new_pitch = delta_pitch + self.current_pitch()
+
+        # Stop the program if this would move the stepper farther than it can move
+        assert new_pitch < self.pitch_axis_degrees
+        assert 0 < new_pitch
+
+        steps = abs(int(delta_pitch/1.8))
+
+        direction = delta_pitch > 0
+
+        self.pitch_motor.motor_go(direction=direction, steps=steps, stepdelay=STEPDELAY_FAST)
+
+        # TODO: the same thing again but for the yaw axis, taking into account which direction the device is pointed
+
     def loop(self):
-        thingsboard_stff()
+        self.thingsboard_stuff()
+        self.point()
+        time.sleep(0.05)
 
 
 if __name__ == "__main__":
@@ -124,15 +174,8 @@ if __name__ == "__main__":
         engine = Engine()
         engine.home()
         engine.loop()
-    except:
+    except AssertionError:
+        print("Assertion failed.")
+    finally:
         GPIO.output((21, 20, 24, 23), GPIO.LOW)
         GPIO.cleanup()
-
-    # armed = setup()
-
-    # try:
-    #     while engine.armed:
-    #         loop()
-    # except KeyboardInterrupt:
-    #     GPIO.output(constants.ALL_PINS, GPIO.LOW)
-    #     GPIO.cleanup()
