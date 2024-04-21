6c16f9033cfb0bcaaa9b632a77bf5f2601da4d54
JP <102888534+normalize-vectors@users.noreply.github.com>
2024-04-20 21:34:36 -0700

    Delete main/secret.py

--- main/secret.py
+++
@@ -1,5 +0,0 @@
-device_lat = 45.630306  # Degrees, latitude of the device
-device_lon = -122.610941  # Degrees, longitude of the device
-device_alt = 309 * 0.3048  # meters, height of the device above sea level, https://en-us.topographic-map.com/
-key_adsb = "30ea111d16msh15f80fe203bf6dap1bce4bjsn7ad3da2a68d7"
-key_thingsboard = ""
